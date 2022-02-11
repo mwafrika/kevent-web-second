34f609f6f1103620712f781e6b756322396d9cf5
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-02-11 13:47:35 +0200

    remove extra space in the page

--- frontend/src/listExpeditions.jsx
+++ frontend/src/listExpeditions.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import moment from 'moment';
+import './App.css';
 import Pagination from './pagination';
 import Header from './header';
 import Footer from './footer';
@@ -8,15 +9,15 @@ const list = () => {
     <>
       <section className='flex min-h-screen flex-col'>
         <Header />
-        <div className='xl:mr-96 lg:mr-36 md:mr-14 xl:float-right xl:pt-10 md:py-5 md:float-right xxxs:mx-auto xxxs:float-none xxxs:py-10 xxs:py-10 xs:py-10 mt-20'>
-          <h1 className='text-slate-600 font-bold text-3xl md:text-2xl xxxs:text-center xxs:text-center xs:text-center md:text-center lg:text-center xl:text-center'>
+        <div className='xl:mr-96 lg:mr-44 md:mr-20 xl:float-right xl:pt-10 md:py-5 md:float-right xxxs:mx-auto xxxs:float-none xxxs:py-10 xxs:py-10 xs:py-10 mt-20'>
+          <h1 className='text-slate-600 font-bold text-3xl xl:pr-60 md:text-2xl xxxs:text-center xxs:text-center xs:text-center md:text-center lg:text-center xl:text-center'>
             Expeditions
           </h1>
         </div>
-        <div className='flex flex-col w-full md:w-11/12 md:py-4 md:flex-row md:flex-wrap md:gap-x-5 md:gap-y-7 md:mx-auto 2xl:flex 2xl:flex-row 2xl:flex-wrap 2xl:gap-14 2xl:mx-auto 2xl:ml-36 2xl:py-10'>
+        <div className='flex flex-col w-full xl:mx-auto mx-auto md:w-11/12 lg:w-11/12 xl:w-11/12  md:py-4 xl:py-4 lg:py-4 md:flex-row xl:flex-row lg:flex-row md:flex-wrap xl:flex-wrap lg:flex-wrap md:gap-x-5 lg:gap-x-5 md:gap-y-7 lg:gap-y-7 md:mx-auto lg:mx-auto xl:flex 2xl:flex 2xl:flex-row 2xl:flex-wrap xl:gap-x-8 xl:gap-y-10 2xl:mx-auto'>
           <div
-            className='2xl:gap-y-5 2xl:mr-20 col-span-1 2xl:w-1/5 2xl:h-full md:w-15 sm:col-span-1  h-full flex flex-col md:pb-1 sm:pt-5 sm:pb-5 lg:pt-5 lg:pb-1 xl:pt-5 xl:pb-5 xl:mr-12 md:px-2 lg:px-5 
-                                 shadow-lg rounded-lg border-gray-50 md:my-0 md:mx-auto xl:w-1/4 sm:w-9/12 2xl:mx-0 sm:mx-auto sm:my-0  sm:px-6 xs:px-8 xs:mx-5 xxs:px-8 xxs:mx-5  xxxs:px-8 xxxs:mx-5'
+            className='2xl:mr-0 xl:mr-0 xl:w-72 lg:mr-0 col-span-1 2xl:w-80 2xl:h-full md:w-15 sm:col-span-1  h-full flex flex-col md:pb-1 sm:pt-5 sm:pb-5 lg:pt-5 lg:pb-1 xl:pt-5 xl:pb-5 md:px-2 lg:px-5 
+         shadow-lg rounded-lg border-gray-50 md:my-0 md:mx-auto sm:w-9/12 2xl:mx-0 sm:mx-auto sm:my-0  sm:px-6 xs:px-8 xs:mx-5 xxs:px-8 xxs:mx-5  xxxs:px-8 xxxs:mx-5'
           >
             <h1 className='xxxs:text-lg  font-bold text-slate-600 md:text-lg mb-8 md:mb-1 lg:mb-4'>
               Search
@@ -55,11 +56,11 @@ const list = () => {
             <h1 className='text-slate-600 xxxs:text-lg md:text-lg pt-5 md:pt-3 lg:pt-5 font-bold'>
               Types
             </h1>
-            <div className='2xl:gap-y-10 flex flex-row flex-wrap h-auto md:gap-x-6 md:gap-y-2 lg:gap-y-6 xl:gap-x-12 lg:gap-x-4 xl:gap-y-4 lg:py-5 xl:py-5 sm:py-5 md:py-5 xs:py-5 xs:gap-x-12 xs:gap-y-6  xxs:py-5 xxs:gap-x-12 xxs:gap-y-6  xxxs:py-5 xxxs:gap-x-1 xxxs:gap-y-3 sm:gap-x-12 sm:gap-y-6 py-2 px-2'>
+            <div className='flex flex-row flex-wrap h-auto md:gap-x-6 md:gap-y-2 xl:gap-x-12 lg:gap-x-4 xl:gap-y-4 lg:py-5 xl:py-5 sm:py-5 md:py-5 xs:py-5 xs:gap-x-12 xs:gap-y-6  xxs:py-5 xxs:gap-x-12 xxs:gap-y-6  xxxs:py-5 xxxs:gap-x-1 xxxs:gap-y-3 sm:gap-x-12 sm:gap-y-6 py-2 px-2'>
               <input
                 type='button'
                 value='Tous'
-                className='bg-slate-600 text-white focus:outline-none focus:shadow-outline border border-gray-300 lg:py-1 xl:py-1 sm:py-1 rounded-full py-2 px-4 lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal
+                className=' bg-slate-600 text-white focus:outline-none focus:shadow-outline border border-gray-300 lg:py-1 xl:py-1 sm:py-1 rounded-full py-2 px-4 lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal
                 hover:bg-slate-600 hover:text-white cursor-pointer
                 active:bg-slate-600 active:text-white
                 '
@@ -67,7 +68,7 @@ const list = () => {
               <input
                 type='button'
                 value='Fruits'
-                className='bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 px-4 lg:py-1 xl:py-1 sm:py-1 lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
+                className=' bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 px-4 lg:py-1 xl:py-1 sm:py-1 lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
               />
               <input
                 type='button'
@@ -77,17 +78,17 @@ const list = () => {
               <input
                 type='button'
                 value='Legumes'
-                className='bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 lg:py-1 px-4 block lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12  xl:py-1 sm:py-1 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
+                className=' bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border border-gray-300 rounded-full py-2 lg:py-1 px-4 block lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12  xl:py-1 sm:py-1 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
               />
               <input
                 type='button'
                 value='Cereals'
-                className='bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border xl:py-1 sm:py-1 border-gray-300 rounded-full py-2 px-4 lg:py-1 block lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
+                className=' bg-slate-300 text-slate-700 focus:outline-none focus:shadow-outline border xl:py-1 sm:py-1 border-gray-300 rounded-full py-2 px-4 lg:py-1 block lg:w-5/12 xl:w-15 sm:w-2/6 md:w-5/12 appearance-none leading-normal hover:bg-slate-600 hover:text-white cursor-pointer'
               />
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className=' h-full md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto xl:w-72 lg:w-48 w-80 2xl:w-98 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -114,7 +115,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='2xl:w-98 xl:w-72 h-full md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-48 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -141,7 +142,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='2xl:w-98 h-full md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='2xl:w-98 h-full md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-48 xl:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -168,7 +169,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='h-full xl:w-72 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -195,7 +196,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='h-full 2xl:w-98 xl:w-72 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -222,7 +223,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='h-full 2xl:w-98 xl:w-72 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -249,7 +250,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='h-full xl:w-72 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
@@ -276,7 +277,7 @@ const list = () => {
             </div>
           </div>
           <div
-            className='h-full 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
+            className='h-full xl:w-72 2xl:w-98 md:w-15 xxxs:w-4/5 xxxs:h-full xxxs:mx-auto xxs:w-4/5 xxs:h-full xxs:mx-auto xs:mx-auto lg:w-72 w-80 mt-10 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110
        cursor-pointer md:mt-0 md:h-full lg:mt-0 lg:h-full shadow-lg rounded-lg
        '
           >
