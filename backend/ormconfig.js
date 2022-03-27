0aa33a54b871fe2d1fb4eb4fbb3a3dd83eda4e31
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-03-27 19:29:19 +0300

    Fix connection error

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,7 +1,13 @@
 const Connection = [
   {
     type: 'postgres',
-    url: 'postgres://kevent:kevent@localhost:5432/keventDB',
+    url: 'postgres://yxocnxjmhyijxg:526c5ebabafb1a2f290c6957b7c2258d4e8b9fe1ef318bd56ecec420f13b7832@ec2-44-194-92-192.compute-1.amazonaws.com:5432/df7ueuvmk7b93d',
+    ssl: true,
+    extra: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     synchronize: true,
     logging: false,
     entities: ['src/entity/**/*.ts'],
@@ -15,13 +21,7 @@ const Connection = [
   },
   {
     type: 'postgres',
-    url: 'postgres://yxocnxjmhyijxg:526c5ebabafb1a2f290c6957b7c2258d4e8b9fe1ef318bd56ecec420f13b7832@ec2-44-194-92-192.compute-1.amazonaws.com:5432/df7ueuvmk7b93d',
-    ssl: true,
-    extra: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    url: 'postgres://kevent:kevent@localhost:5432/keventDB',
     synchronize: true,
     logging: false,
     entities: ['src/entity/**/*.ts'],
