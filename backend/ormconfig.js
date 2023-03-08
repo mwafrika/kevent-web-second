1ac7b6a44138ad3fd2524ef52d50d70b8b4c3f67
mwafrika <mwafrikajosue@gmail.com>
2023-03-08 11:32:55 +0200

    Remove commented codes

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,9 +1,3 @@
-// const { flattenDiagnosticMessageText } = require("typescript");
-
-// var DATABASE_URL = require("./src/config").DATABASE_URL;
-// var DEV_DATABASE_URL = require("./src/config").DEV_DATABASE_URL;
-
-console.log(process.env.DATABASE_URL, "DATABASE_URL");
 const Connection = [
   {
     environment: "production",
@@ -15,7 +9,7 @@ const Connection = [
         rejectUnauthorized: false,
       },
     },
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
@@ -30,7 +24,7 @@ const Connection = [
     environment: "development",
     type: "postgres",
     url: process.env.DEV_DATABASE_URL,
-    synchronize: false,
+    synchronize: true,
     logging: false,
     entities: ["src/entity/**/*.ts"],
     migrations: ["src/migration/**/*.ts"],
